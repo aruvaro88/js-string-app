f90e1c735b6963b40ba6663307cdc65d28005f63
aruvaro88 <aruvaro@gmail.com>
2025-03-16 18:42:58 +0100

    logic improvements

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Variables
     const addButton = document.getElementById("add-button");
     const modal = document.getElementById("modal");
     const cancelButton = document.getElementById("cancel-button");
@@ -9,7 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const deleteButton = document.getElementById("delete-button");
     const undoButton = document.getElementById("undo-button")
     let actionStack = []
-    let lastDeletedItem = null
     
     addButton.addEventListener("click", () => {
         modal.style.display = "flex"; 
@@ -23,40 +21,59 @@ document.addEventListener("DOMContentLoaded", () => {
     
     addItemButton.addEventListener("click", () => {
         const text = textInput.value.trim(); 
+        const errorMessage = document.getElementById("error-message");
         
-        if (text) {
-            const li = document.createElement("li");
-            li.textContent = text;
-            li.classList.add("list-item");
+        if (text === "") {
+            errorMessage.style.display = "block";
+            return;
+        }else{
+            const item = document.createElement("span");
+            item.textContent = text;
             
-            li.addEventListener("click", () => {
-                li.classList.toggle("selected"); 
+            item.classList.add("list-item");
+            
+            item.addEventListener("click", () => {
+                item.classList.toggle("selected"); 
                 deleteButton.disabled = !document.querySelector(".selected");
             });
+
+            item.addEventListener("dblclick", ()=>{
+                itemList.removeChild(item)
+                actionStack.push({action:"delete", item: item})
+            })
             
-            itemList.appendChild(li);
-            actionStack.push({action: "add", item: li})
+            itemList.appendChild(item);
+            actionStack.push({action: "add", item: item})
+            errorMessage.style.display= "none"
         } 
         textInput.value = ""; 
         modal.style.display = "none"; 
     });
 
     deleteButton.addEventListener("click", () => {
-        selectedItem = document.querySelector(".selected");
-        if (selectedItem) {
-            lastDeletedItem = selectedItem
-            itemList.removeChild(selectedItem)
-            actionStack.push({action: "delete", item: selectedItem})
+        selectedItems = document.querySelectorAll(".selected");
+        if(selectedItems.length>0){
+            const deletedItems = []
+            selectedItems.forEach((item)=>{
+                deletedItems.push(item)
+                itemList.removeChild(item)
+            })
+        actionStack.push({action: "delete", item: deletedItems})
         }
     });
 
     undoButton.addEventListener("click", ()=> {
         const lastAction = actionStack.pop()
+
         if(lastAction){
             if (lastAction.action === "add") {
                 itemList.removeChild(lastAction.item);
             } else if (lastAction.action === "delete") {
-                itemList.appendChild(lastAction.item);
+                const itemsToRestore = Array.isArray(lastAction.item) ? lastAction.item : [lastAction.item];
+                 itemsToRestore.forEach(elm => {
+                    elm.classList.remove("selected")
+                    itemList.appendChild(elm);                   
+                });
             }
         }
     })
